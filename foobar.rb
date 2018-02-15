75be06e9e09d85a23c14de4b5c5aef9e2c6cdbe4
Leon Kwak <lkwak783@gmail.com>
2018-02-15 01:54:37 -0800

    ken is the real daddy

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,12 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a_int = a.map{ |i| i.to_i }.map{ |i| i+2 }.select{ |i| i % 2 == 0 }
+    no_duplicates = a_int.uniq
+    new_arr = no_duplicates.reject{ |i| i >= 10}
+
+    sum = 0
+    new_arr.each{ |i| sum += i }
+    sum
   end
 end
